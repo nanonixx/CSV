79b082042d9da818db77076393f4c3c251634172
nanonixx <nhid@elpuig.xeill.net>
2021-02-02 22:49:14 +0100

    yastà

--- src/main/java/Menu.java
+++ src/main/java/Menu.java
@@ -12,10 +12,10 @@ public class Menu {
         String menu = "";
 
         while (!(menu.equals("0"))){
-        System.out.println("1.  Naixements al barri del Poblenou");
+        System.out.println("\n1.  Naixements al barri del Poblenou");
         System.out.println("2.  Número de naixements de mares nascudes al Marroc ");
         System.out.println("3.  Naixements als districtes amb la paraula \"Sant\" al seu nom");
-        System.out.println("4.  Naixaments de mares nascudes a països que comencen per 'A'");
+        System.out.println("4.  Naixements de mares nascudes a països que comencen per 'A'");
         System.out.println("5.  Comprovar si van haver naixements al 2019 a un barri concret de Barcelona");
         System.out.println("\n0. Sortir");
 
@@ -43,7 +43,7 @@ public class Menu {
 
             case "4":
                 llista.stream()
-                        .filter(naixement -> naixement.getLloc_de_naixement().equals("A"))
+                        .filter(naixement -> naixement.getLloc_de_naixement().contains("A"))
                         .forEach(System.out::println);
                 break;
 
